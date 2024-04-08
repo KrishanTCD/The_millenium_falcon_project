d782a563635e93e0937dba74de84941dfd0ea7e2
Krishan_Kant_Xero <146117329+KrishanTCD@users.noreply.github.com>
2024-04-08 03:50:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-username="Xero_Sam"
-password="Dublin@2024"
-client_id="TlpetD7ESWBtBRXG3aKv1A"
-client_secret="aASw1yOgb3iBBypZdEb6BHBp4VydzQ"
+username=""
+password=""
+client_id=""
+client_secret=""
 
